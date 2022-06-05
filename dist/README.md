d03e7da1ab175f12c92c113fbb4eb0b344d23a01
Brandon Benson <brandonbenson09@gmail.com>
2022-06-05 17:04:15 -0400

    added validationg for the questions prompt

---
+++ dist/README.md
@@ -0,0 +1,38 @@
+# asd
+
+  
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+
+  ## Description
+  asd
+
+  ## Installation
+  asdas
+
+  ## Usage
+  d
+
+  
+
+  ## Contributing
+  undefined
+
+  ## Tests
+  undefined
+
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/asd  
+  Email: asd
+
+  ## Credits
+  i
--- index.js
+++ index.js
@@ -10,36 +10,84 @@ const questions = [
     type: "input",
     name: "name",
     message: "What is your name?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("You MUST enter your name! (Required");
+        return false;
+      }
+    },
   },
   // project name?
   {
     type: "input",
     name: "title",
     message: "What is the title of your project? (Required)",
+    validate: (projectName) => {
+      if (projectName) {
+        return true;
+      } else {
+        console.log("You MUST enter your projects name!");
+        return false;
+      }
+    },
   },
   {
     // Description of project
     type: "input",
     name: "description",
     message: "Provide a description of this project! (Required)",
+    validate: (projectDescription) => {
+      if (projectDescription) {
+        return true;
+      } else {
+        console.log("You MUST provide a project description!");
+        return false;
+      }
+    },
   },
   {
     // Install instructions
     type: "input",
     name: "installation",
     message: "How can someone install your project? (Required)",
+    validate: (installInstructions) => {
+      if (installInstructions) {
+        return true;
+      } else {
+        console.log("You MUST describe installation details!");
+        return false;
+      }
+    },
   },
   {
     // how to use this project
     type: "input",
     name: "usage",
     message: "How do you use this project? (Required)",
+    validate: (usageDetails) => {
+      if (usageDetails) {
+        return true;
+      } else {
+        console.log("You MUST provide how to use this project!");
+        return false;
+      }
+    },
   },
   {
     // test instructions
     type: "input",
     name: "testing",
     message: "How to test this project? (Required)",
+    validate: (testProject) => {
+      if (testProject) {
+        return true;
+      } else {
+        console.log("Please provide how to test this project!");
+        return false;
+      }
+    },
   },
   {
     // license input
@@ -47,18 +95,42 @@ const questions = [
     name: "licensing",
     message: "Choose a license for your project! (Required)",
     choices: [],
+    validate: (licenseInput) => {
+      if (licenseInput) {
+        return true;
+      } else {
+        console.log("You MUST check one of the options to continue!");
+        return false;
+      }
+    },
   },
   {
     // github username
     type: "input",
     name: "github",
     message: "Enter your Github Username! (Required)",
+    validate: (githubName) => {
+      if (githubName) {
+        return true;
+      } else {
+        console.log("You MUST provide your GitHub Username!");
+        return false;
+      }
+    },
   },
   {
     // user email address
     type: "input",
     name: "email",
-    message: "Would you like to add an email?",
+    message: "Would you like to add an email? (Required)",
+    validate: (emailAddress) => {
+      if (emailAddress) {
+        return true;
+      } else {
+        console.log("You MUST provide your email to continue!");
+        return false;
+      }
+    },
   },
 ];
 
@@ -72,7 +144,9 @@ function writeToFile(data) {
       }
       conclude({
         ok: true,
-        message: console.log(""),
+        message: console.log(
+          "Refer to dist folder to see your created README.md"
+        ),
       });
     });
   });
@@ -84,6 +158,13 @@ const init = () => {
 };
 
 // Function call to initialize app
-init().then((userInput) => {
-  return;
-});
+init()
+  .then((userInput) => {
+    return generateMarkdown(userInput);
+  })
+  .then((userReadMe) => {
+    return writeToFile(userReadMe);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
